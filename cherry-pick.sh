ae838ea4fcc9aa459011a8eae78d67491fe2a82f
Esteban Guzzo <estebanguzzo@gmail.com>
2015-05-22 03:15:53 -0300

    Add build patch

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -18,3 +18,11 @@ then
   cd ../..
 fi
 
+if [ "$REPO_BRANCH" = "cm-11.0" ] || [ "$DEVICE" = "degaswifi" ]
+then
+  echo -e $CL_BLU"Cherrypicking build patches"$CL_RST
+  cd build/tools
+  git fetch https://github.com/MaxiCM/android_build cm-11.0
+  git cherry-pick 2496b15e9547e60b471484b812a28339b366fb70
+  cd ../..
+fi
