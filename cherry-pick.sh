022aee56bbe46e2359c1def368d2b3aa31761745
Esteban Guzzo <estebanguzzo@gmail.com>
2015-05-29 23:28:33 -0300

    Remove hardware_libhardware and system_core patch
    Now use include headers :)

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -2,24 +2,6 @@
 
 if [ "$REPO_BRANCH" = "cm-11.0" ] || [ "$DEVICE" = "degaswifi" ]
 then
-  echo -e $CL_BLU"Cherrypicking libhardware patches"$CL_RST
-  cd hardware/libhardware
-  git fetch https://github.com/MaxiCM/android_hardware_libhardware cm-11.0
-  git cherry-pick 85995fe1d648c8c2407d815398afa8bcc2f55428
-  cd ../..
-fi
-
-if [ "$REPO_BRANCH" = "cm-11.0" ] || [ "$DEVICE" = "degaswifi" ]
-then
-  echo -e $CL_BLU"Cherrypicking multimedia patches"$CL_RST
-  cd system/core
-  git fetch https://github.com/MaxiCM/android_system_core cm-11.0
-  git cherry-pick a8453a68a8fa2572699073c1eba020acb44ab970
-  cd ../..
-fi
-
-if [ "$REPO_BRANCH" = "cm-11.0" ] || [ "$DEVICE" = "degaswifi" ]
-then
   echo -e $CL_BLU"Cherrypicking build patches"$CL_RST
   cd build/tools
   git fetch https://github.com/MaxiCM/android_build cm-11.0
