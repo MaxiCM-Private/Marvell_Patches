2141f1172b6182679de897ca4309d093e3947690
Esteban Guzzo <estebanguzzo@gmail.com>
2015-07-13 00:51:47 -0300

    Update native patches and remove base patches

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -4,19 +4,8 @@ if [ "$REPO_BRANCH" = "cm-11.0" ] || [ "$DEVICE" = "degaswifi" ]
 then		
   echo -e $CL_BLU"Cherrypicking native patches"$CL_RST		
   cd frameworks/native		
-  git fetch https://github.com/MaxiCM/android_frameworks_native patch-4
-  git cherry-pick a65cd1e45f9b5221529804a848520f55a4467d90
-  git cherry-pick 78c2dae904d996911b53003c8973c99fe34ee288
-  git cherry-pick fbc50cf58b9a1ac77290405eab669477c205c6de
-  cd ../..		
-fi
-
-if [ "$REPO_BRANCH" = "cm-11.0" ] || [ "$DEVICE" = "degaswifi" ]		
-then		
-  echo -e $CL_BLU"Cherrypicking base patches"$CL_RST		
-  cd frameworks/base		
-  git fetch https://github.com/MaxiCM/android_frameworks_base cm-11.0
-  git cherry-pick ffbd270ae534adaa5f5a2099e18214939324e9d8
+  git fetch https://github.com/MaxiCM/android_frameworks_native patch-5
+  git cherry-pick def093e9da5ffbb84514577a5c75db1d5fda16d5
   cd ../..		
 fi
 
