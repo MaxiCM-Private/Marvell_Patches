092a5be2cbd1d3cf152e0662a972ecdece1bc575
Esteban Guzzo <estebanguzzo@gmail.com>
2015-07-14 21:33:37 -0300

    Ups

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [ "$REPO_BRANCH" = "cm-11.0" ] || [ "$DEVICE" = "degaswifi" ]		
-then		
-  echo -e $CL_BLU"Cherrypicking native patches"$CL_RST		
-  cd frameworks/native		
-  git fetch https://github.com/MaxiCM/android_frameworks_native patch-6
-  git cherry-pick acde48d134a68ec0bc8dd86efdda577e4e834a54
-  cd ../..		
+if [ "$REPO_BRANCH" = "cm-11.0" ] || [ "$DEVICE" = "degaswifi" ]
+ then
+  echo -e $CL_BLU"Cherrypicking build patches"$CL_RST
+  cd build/tools
+  git fetch https://github.com/MaxiCM/android_build cm-11.0
+  git cherry-pick 2496b15e9547e60b471484b812a28339b366fb70
+  cd ../..
 fi
 
 if [ "$REPO_BRANCH" = "cm-11.0" ] || [ "$DEVICE" = "degaswifi" ]
